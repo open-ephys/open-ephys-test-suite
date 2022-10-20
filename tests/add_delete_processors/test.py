35edb9425df71203b730deb21a077a9bf7acaef5
Pavel Kulik <pavelkulik@hotmail.com>
2022-10-20 13:11:02 -0700

    Add first test using new format

---
+++ tests/add_delete_processors/test.py
@@ -0,0 +1,86 @@
+import platform
+import os
+
+from open_ephys.control import OpenEphysHTTPServer
+
+"""
+Test Name: Add/Delete Processor
+Test Description: Add and delete a processor from the signal chain
+"""
+
+def test(gui, params):
+
+    # Store test results to report in a dictionary
+    results = {}
+
+    # Load default FileReader config chain
+    gui.load(params['cfg_path'])
+
+    # Get list of processors in signal chain
+    numProcessorsBeforeAdd = len(gui.get_processors())
+
+    # Add a processor to end of signal chain
+    test_name = 'Add processor'
+    gui.add_processor("Bandpass Filter")
+
+    # Check if add was successful
+    numProcessorsAfterAdd = len(gui.get_processors())
+
+    if numProcessorsAfterAdd == numProcessorsBeforeAdd + 1:
+        results[test_name] = "PASSED"
+    else:
+        results[test_name] = "FAILED\n\tProcessor count before add: " + str(numProcessorsBeforeAdd) + " after: " + str(numProcessorsAfterAdd)
+
+    # Delete the most recently added processor
+    mostRecentlyAddedProcessorId = max(gui.get_processors(), key=lambda processor: processor['id'])['id']
+    gui.delete_processor(mostRecentlyAddedProcessorId)
+
+    # Check if delete was successful
+    numProcessorsBeforeDelete = numProcessorsAfterAdd
+    numProcessorsAfterDelete = len(gui.get_processors())
+
+    if numProcessorsAfterDelete == numProcessorsBeforeDelete - 1:
+        results['Delete processor'] = "PASSED"
+    else:
+        results['Delete processor'] = "FAILED\n\tProcessor count before delete: " + str(numProcessorsBeforeDelete) + " after: " + str(numProcessorsAfterDelete)
+
+    # Print summary of results
+    for test, result in results.items():
+        print(test, '-'*(80-len(test)), result)
+
+'''
+================================================================================================================================================
+'''
+
+import sys
+import argparse
+
+from pathlib import Path
+
+if platform.system() == 'Windows':
+    RECORD_PATH = 'C:\\open-ephys\\data'
+elif platform.system() == 'Linux':
+    RECORD_PATH = '<path/to/linux/runner>' #TODO
+else:
+    RECORD_PATH = '<path/to/mac/runner>' #TODO
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--mode', required=True, choices={'local', 'githubactions'})
+    parser.add_argument('--fetch', required=False, default=True, action='store_true')
+    parser.add_argument('--address', required=False, type=str, default='http://127.0.0.1')
+    parser.add_argument('--cfg_path', required=False, type=str, default=os.path.join(Path(__file__).resolve().parent, 'file_reader_config.xml'))
+    parser.add_argument('--acq_time', required=False, type=int, default=2)
+    parser.add_argument('--rec_time', required=False, type=int, default=5)
+    parser.add_argument('--num_rec', required=False, type=int, default=1)
+    parser.add_argument('--num_exp', required=False, type=int, default=1)
+    parser.add_argument('--prepend_text', required=False, type=str, default='')
+    parser.add_argument('--base_text', required=False, type=str, default='')
+    parser.add_argument('--append_text', required=False, type=str, default='')
+    parser.add_argument('--parent_directory', required=False, type=str, default='RECORD_PATH')
+    parser.add_argument('--engine', required=False, type=str, default='engine=0')
+
+    params = vars(parser.parse_args(sys.argv[1:]))
+
+    test(OpenEphysHTTPServer(), params)
