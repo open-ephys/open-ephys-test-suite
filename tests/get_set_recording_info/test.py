c42a85ae773975ade4ac269397dd77e4071ec555
Pavel Kulik <pavelkulik@hotmail.com>
2022-10-21 17:11:43 -0700

    Update get_set_recording_info

--- tests/get_set_recording_info/test.py
+++ tests/get_set_recording_info/test.py
@@ -1,31 +1,101 @@
 from open_ephys.control import OpenEphysHTTPServer
 from open_ephys.analysis import Session
 
-import config
+import time 
 
-def test(gui):
+def test(gui, params):
 
     # Fetch fresh data if needed
-    if config.test_params['fetch']: 
+    if params['fetch']: 
 
-        gui.set_prepend_text(config.test_params['prepend_text'])
-        gui.set_base_text(config.test_params['base_text'])
-        gui.set_append_text(config.test_params['append_text'])
-        gui.set_parent_dir(config.test_params['parent_directory'])
+        results = {}
 
-        print("Test prepend text: ", "PASSED" if gui.get_recording_info('prepend_text') == config.test_params['prepend_text'] else "FAILED")
-        print("Test base text: ", "PASSED" if gui.get_recording_info('base_text') == config.test_params['base_text'] else "FAILED")
-        print("Test append text: ", "PASSED" if gui.get_recording_info('append_text') == config.test_params['append_text'] else "FAILED")
-        print("Test parent dir: ", "PASSED" if gui.get_recording_info('parent_directory') == config.test_params['parent_directory'] else "FAILED")
+        gui.load(params['cfg_path'])
 
-        #TODO: Record data and then check that the recorded folder has the desired directory name:
+        gui.set_prepend_text(params['prepend_text'])
+        gui.set_base_text(params['base_text'])
+        gui.set_append_text(params['append_text'])
+        gui.set_parent_dir(params['parent_directory'])
 
-        #gui.quit()
+        # Set all record node engines
+        for node in gui.get_processors("Record Node"):
+            gui.set_record_engine(node['id'], params['engine'])
 
-    # Validate results
-    
-    #TODO: Parse recorded data folder name and confirm that it matches the desired name
-    #session = Session(gui.get_latest_recordings(config.test_params['parent_directory'])[0])
+        # Add record node after Spike Viewer
+        gui.add_processor("Record Node")
+
+        nodeDirectories = {}
+        for node in gui.get_recording_info()['record_nodes']:
+            nodeDirectories[node['node_id']] = node['parent_directory']
+            print("%d : %s" % (node['node_id'], node['parent_directory']))
+
+        # Only latest RecordNode will have the newly set parent directory
+        testName = 'Set Recording Locations'
+
+        for node in gui.get_recording_info()['record_nodes']:
+            print(node['parent_directory'])
+
+        # Run some actions and record data
+        for n in range(params['num_exp']):
+
+            #Start a new directory only for the third experiment
+            if n == 2:
+                gui.set_start_new_dir()
+
+            for _ in range(params['num_rec']):
+
+                gui.acquire()
+                time.sleep(params['acq_time'])
+                gui.record(params['rec_time'])
+
+            gui.idle()
+
+    #Find the expected recordings
+    for node, dir in nodeDirectories.items():
+        if len(gui.get_latest_recordings(dir, count=2)) == 2:
+            results[testName] = "PASSED"
+        else:
+            results[testName] = "FAILED\n\tExpected 2 recordings in %s, found %d" % (dir, len(gui.get_latest_recordings(dir, count=2)))
+
+    return results
+
+'''
+================================================================================================================================================
+'''
+import os
+import sys
+import argparse
+import platform
+
+from pathlib import Path
+
+if platform.system() == 'Windows':
+    RECORD_PATH = 'C:\\open-ephys\\data'
+elif platform.system() == 'Linux':
+    RECORD_PATH = '<path/to/linux/runner>' #TODO
+else:
+    RECORD_PATH = '<path/to/mac/runner>' #TODO
 
 if __name__ == '__main__':
-    test(OpenEphysHTTPServer(config.test_params['address']))
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--mode', required=True, choices={'local', 'githubactions'})
+    parser.add_argument('--fetch', required=False, default=True, action='store_true')
+    parser.add_argument('--address', required=False, type=str, default='http://127.0.0.1')
+    parser.add_argument('--cfg_path', required=False, type=str, default=os.path.join(Path(__file__).resolve().parent, 'file_reader_config.xml'))
+    parser.add_argument('--acq_time', required=False, type=int, default=2)
+    parser.add_argument('--rec_time', required=False, type=int, default=5)
+    parser.add_argument('--num_rec', required=False, type=int, default=1)
+    parser.add_argument('--num_exp', required=False, type=int, default=3)
+    parser.add_argument('--prepend_text', required=False, type=str, default='alice')
+    parser.add_argument('--base_text', required=False, type=str, default='test')
+    parser.add_argument('--append_text', required=False, type=str, default='auto')
+    parser.add_argument('--parent_directory', required=False, type=str, default=RECORD_PATH)
+    parser.add_argument('--engine', required=False, type=str, default='engine=0')
+
+    params = vars(parser.parse_args(sys.argv[1:]))
+
+    results = test(OpenEphysHTTPServer(), params)
+
+    for test, result in results.items():
+        print(test, '-'*(80-len(test)), result)
