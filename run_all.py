258ff87f35eba1d8f0193dcef33689ce71ff719c
Pavel Kulik <pavelkulik@hotmail.com>
2023-02-07 14:39:21 -0800

    Remove launch GUI before running all tests

--- run_all.py
+++ run_all.py
@@ -4,14 +4,10 @@ import glob
 import pathlib
 import time
 
-GUI_EXE = "C:\\Users\\Pavel\\Projects\\Allen\\OpenEphys\\plugin-GUI\\Build\\Release\\open-ephys.exe"
-
 tests = glob.glob(os.path.join(pathlib.Path().absolute(), "tests", "*.py"))
 
 for test in tests:
     print("Running: ", test)
-    if platform.system() == 'Windows':
-        os.startfile(GUI_EXE)
     rc = os.system("python " + test + " --mode local")
     if rc != 0:
         print("Test failed: ", test)
